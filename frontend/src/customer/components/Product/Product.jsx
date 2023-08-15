1a616ae0022ce30b439ec3d1fcfcace5b6d873e6
Saurav <phatesaurav@gmail.com>
2023-08-16 00:13:29 +0530

    error resolution in URL

--- frontend/src/customer/components/Product/Product.jsx
+++ frontend/src/customer/components/Product/Product.jsx
@@ -11,68 +11,9 @@ import FilterListIcon from '@mui/icons-material/FilterList';
 import { Navigate, useLocation, useNavigate } from 'react-router-dom'
 
 const sortOptions = [
-  /* { name: 'Most Popular', href: '#', current: true },
-  { name: 'Best Rating', href: '#', current: false },
-  { name: 'Newest', href: '#', current: false }, */
   { name: 'Price: Low to High', href: '#', current: false },
   { name: 'Price: High to Low', href: '#', current: false },
 ]
-/* const subCategories = [
-  { name: 'Totes', href: '#' },
-  { name: 'Backpacks', href: '#' },
-  { name: 'Travel Bags', href: '#' },
-  { name: 'Hip Bags', href: '#' },
-  { name: 'Laptop Sleeves', href: '#' },
-] */
-
-/* const filters = [
-  {
-    id: 'brand',
-    name: 'Brand',
-    options: [
-      { value: 'apple', label: 'Apple', checked: true },
-      { value: 'google', label: 'Google', checked: false },
-      { value: 'motorola', label: 'Motorola', checked: false },
-      { value: 'oneplus', label: 'OnePlus', checked: false },
-      { value: 'oppo', label: 'Oppo', checked: false },
-      { value: 'poco', label: 'Poco', checked: false },
-      { value: 'realme', label: 'Realme', checked: false },
-      { value: 'samsung', label: 'Samsung', checked: false },
-    ],
-  },
-  {
-    id: 'ram',
-    name: 'RAM',
-    options: [
-      { value: '4gb', label: '4 GB', checked: true },
-      { value: '8gb', label: '8 GB', checked: false },
-      { value: '16gb', label: '16 GB', checked: false },
-    ],
-  },
-  {
-    id: 'storage',
-    name: 'Storage',
-    options: [
-      { value: '64gb', label: '64 GB', checked: false },
-      { value: '128gb', label: '128 GB', checked: false },
-      { value: '256gb', label: '256 GB', checked: false },
-      { value: '512gb', label: '512 GB', checked: false },
-      { value: '1tb', label: '1 TB', checked: false },
-    ],
-  },
-  {
-    id: 'battery',
-    name: 'Battery Capacity',
-    options: [
-      { value: '3000mah', label: '3000 mAh', checked: false },
-      { value: '3500mah', label: '3500 mAh', checked: false },
-      { value: '4000mah', label: '4000 mAh', checked: false },
-      { value: '4500mah', label: '4500 mAh', checked: false },
-      { value: '5000mah', label: '5000 mAh', checked: false },
-      { value: '6000mah', label: '6000 mAh', checked: false },
-    ],
-  },
-] */
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -81,7 +22,7 @@ function classNames(...classes) {
 export default function Product() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
   const location=useLocation()
-  const navigate=useNavigate()
+  const navigate=useNavigate();
 
   const handleFilter=(value, sectionId)=>{
     
@@ -107,7 +48,7 @@ export default function Product() {
     }
 
     const query=searchParams.toString();
-      navigate({search:`?${query}`})
+    navigate({search:`?${query}`})
   }
 
   const handleRadioFilterChange=(e,sectionId)=>{
@@ -161,16 +102,6 @@ export default function Product() {
 
                   {/* Filters */}
                   <form className="mt-4 border-t border-gray-200">
-                    {/* <h3 className="sr-only">Categories</h3>
-                    {<ul role="list" className="px-2 py-3 font-medium text-gray-900">
-                      {subCategories.map((category) => (
-                        <li key={category.name}>
-                          <a href={category.href} className="block px-2 py-3">
-                            {category.name}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>} */}
 
                     {filters.map((section) => (
                       <Disclosure as="div" key={section.id} className="border-t border-gray-200 px-4 py-6">
@@ -193,7 +124,6 @@ export default function Product() {
                                 {section.options.map((option, optionIdx) => (
                                   <div key={option.value} className="flex items-center">
                                     <input
-                                      onChange={()=>handleFilter(option.value, section.id)}
                                       id={`filter-mobile-${section.id}-${optionIdx}`}
                                       name={`${section.id}[]`}
                                       defaultValue={option.value}
@@ -292,19 +222,12 @@ export default function Product() {
 
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-6">
               <div>
-              <div className='py-10 flex justify-between items-center'>
-                <h1 className='text-lg opacity-50 font-bold'>Filters</h1>
-                <FilterListIcon/>
-              </div>
-              <form className="hidden lg:block">
-                {/* <h3 className="sr-only">Categories</h3>
-                {<ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
-                  {subCategories.map((category) => (
-                    <li key={category.name}>
-                      <a href={category.href}>{category.name}</a>
-                    </li>
-                  ))}
-                </ul>} */}
+                <div className='py-10 flex justify-between items-center'>
+                  <h1 className='text-lg opacity-50 font-bold'>Filters</h1>
+                  <FilterListIcon/>
+                </div>
+                
+                <form className="hidden lg:block">
 
                 {filters.map((section) => (
                   <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
@@ -327,6 +250,7 @@ export default function Product() {
                             {section.options.map((option, optionIdx) => (
                               <div key={option.value} className="flex items-center">
                                 <input
+                                  onChange={()=>handleFilter(option.value, section.id)}
                                   id={`filter-${section.id}-${optionIdx}`}
                                   name={`${section.id}[]`}
                                   defaultValue={option.value}
@@ -348,13 +272,13 @@ export default function Product() {
                     )}
                   </Disclosure>
                 ))}
+
                 {singleFilter.map((section) => (
                   <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
                     {({ open }) => (
                       <>
                         <h3 className="-my-3 flow-root">
                           <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                            {/* <span className="font-medium"></span> */}
                               <FormLabel sx={{color:"black"}} className='text-gray-900' id="demo-radio-buttons-group-label">{section.name}</FormLabel>
                             <span className="ml-6 flex items-center">
                               {open ? (
