7874df0d61d32be34eceba0725f0bfa5d1c3fe9f
Akash Verma <omakash3110@gmail.com>
2023-08-15 14:09:32 +0530

    added functionality to corousel

--- frontend/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ frontend/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const HomeSectionCard = () => {
+const HomeSectionCard = ({ product }) => {
   return (
-    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3'>
-        <div className='h-[13rem] w-[10rem]'>
-            <img className='object-cover object-top w-full h-full' src="https://s3no.cashify.in/cashify/product/img/xxhdpi/d8394bf8-e922.jpg?p=es3sq&s=es" alt="" />
-        </div>
+    <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border">
+      <div className="h-[13rem] w-[10rem]">
+        <img
+          className="object-cover object-top w-full h-full"
+          src={product.imageUrl}
+          alt=""
+        />
+      </div>
 
-        <div className='p-4'>
-            <h3 className='text-lg font-medium text-grey-900'>Apple iPhone 11 - Refurbished</h3>
-            <p className='mt-2 text-sm text-grey-500'>Fair, 4 GB / 64 GB, Black</p>
-        </div>
+      <div className="p-4">
+        <h3 className="text-lg font-medium text-grey-900">
+          {product.brand}
+        </h3>
+        <p className="mt-2 text-sm text-grey-500">{product.title}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeSectionCard
+export default HomeSectionCard;
--- frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,36 +1,77 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import { Button } from '@mui/material';
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { Button } from "@mui/material";
+import { smartphones } from "../../../Data/smartphones";
 
 const HomeSectionCarousel = () => {
-    const responsive = {
-        0: { items: 1 },
-        720: { items: 3 },
-        1024: { items: 5.5 },
-    };
+  const [activeIndex,setActiveIndex]=useState(0);
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
 
-    const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
-    return (
-        <div className='relative px-4 lg:px-8'>
-            <div className='relative p-5'>
-                <AliceCarousel
-                    items={items}
-                    disableButtonsControls
-                    infinite
-                    responsive={responsive}
-                />
-                <Button variant='contained' className='z-50 bg-white' sx={{ position: 'absolute', top: '8rem', right: '0rem', transform: 'translateX(50%) rotate(90deg)', bgcolor: 'white' }} aria-label='next'>
-                    <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
-                </Button>
+  };
 
-                <Button variant='contained' className='z-50 bg-white' sx={{ position: 'absolute', top: '8rem', left: '0rem', transform: 'translateX(50%) rotate(90deg)', bgcolor: 'white' }} aria-label='next'>
-                    <KeyboardArrowLeftIcon sx={{ transform: 'rotate(-90deg)', color: 'black' }} />
-                </Button>
-            </div>
-        </div>
-    )
-}
+  const slidePrev=()=>setActiveIndex(activeIndex-1)
+  const slideNext=()=>setActiveIndex(activeIndex+1)
 
-export default HomeSectionCarousel
+  const syncActiveIndex=({item})=>setActiveIndex(item)
+
+  const items = smartphones.slice(0,6).map((item) => (
+    <HomeSectionCard product={item}/>
+  ));
+  return (
+    <div className="border">
+      <div className="relative p-5">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          // infinite
+          responsive={responsive}
+          disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+        {activeIndex !== items.length-5 && <Button
+          variant="contained"
+          className="z-50 bg-white"
+          onClick={slideNext}
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            right: "0rem",
+            transform: "translateX(50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+          aria-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button>}
+
+        {activeIndex !== 0 &&<Button
+        onClick={slidePrev}
+          variant="contained"
+          className="z-50 bg-white"
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            left: "0rem",
+            transform: "translateX(-50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+          aria-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(-90deg)", color: "black" }}
+          />
+        </Button>}
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarousel;
