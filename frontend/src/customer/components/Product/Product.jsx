4202e9f54ddf85afc5fa0a1567745f434bb3195d
Saurav <phatesaurav@gmail.com>
2023-08-15 20:26:43 +0530

    added filters and URL extension

--- frontend/src/customer/components/Product/Product.jsx
+++ frontend/src/customer/components/Product/Product.jsx
@@ -5,22 +5,27 @@ import { XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
 import ProductCard from './ProductCard'
 import { smartphones } from '../../../Data/smartphones'
+import { filters, singleFilter } from './FilterData'
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom'
 
 const sortOptions = [
-  { name: 'Most Popular', href: '#', current: true },
+  /* { name: 'Most Popular', href: '#', current: true },
   { name: 'Best Rating', href: '#', current: false },
-  { name: 'Newest', href: '#', current: false },
+  { name: 'Newest', href: '#', current: false }, */
   { name: 'Price: Low to High', href: '#', current: false },
   { name: 'Price: High to Low', href: '#', current: false },
 ]
-// const subCategories = [
-//   { name: 'Totes', href: '#' },
-//   { name: 'Backpacks', href: '#' },
-//   { name: 'Travel Bags', href: '#' },
-//   { name: 'Hip Bags', href: '#' },
-//   { name: 'Laptop Sleeves', href: '#' },
-// ]
-const filters = [
+/* const subCategories = [
+  { name: 'Totes', href: '#' },
+  { name: 'Backpacks', href: '#' },
+  { name: 'Travel Bags', href: '#' },
+  { name: 'Hip Bags', href: '#' },
+  { name: 'Laptop Sleeves', href: '#' },
+] */
+
+/* const filters = [
   {
     id: 'brand',
     name: 'Brand',
@@ -67,7 +72,7 @@ const filters = [
       { value: '6000mah', label: '6000 mAh', checked: false },
     ],
   },
-]
+] */
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -75,6 +80,43 @@ function classNames(...classes) {
 
 export default function Product() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+  const location=useLocation()
+  const navigate=useNavigate()
+
+  const handleFilter=(value, sectionId)=>{
+    
+    const searchParams=new URLSearchParams(location.search)
+
+    let filterValue=searchParams.getAll(sectionId)
+
+    if(filterValue.length>0 && filterValue[0].split(",").includes(value)){
+      filterValue=filterValue[0].split(",").filter((item)=>item!==value);
+
+      if(filterValue.length===0){
+        searchParams.delete(sectionId)
+        console.log("yes delete")
+      }
+      console.log("includes ,",value,sectionId,filterValue)
+    }
+    else{
+      filterValue.push(value)
+    }
+
+    if(filterValue.length>0){
+      searchParams.set(sectionId, filterValue.join(","));      
+    }
+
+    const query=searchParams.toString();
+      navigate({search:`?${query}`})
+  }
+
+  const handleRadioFilterChange=(e,sectionId)=>{
+    const searchParams=new URLSearchParams(location.search)
+
+    searchParams.set(sectionId,e.target.value)
+    const query=searchParams.toString();
+    navigate({search:`?${query}`})
+  }
 
   return (
     <div className="bg-white">
@@ -119,8 +161,8 @@ export default function Product() {
 
                   {/* Filters */}
                   <form className="mt-4 border-t border-gray-200">
-                    <h3 className="sr-only">Categories</h3>
-                    {/* <ul role="list" className="px-2 py-3 font-medium text-gray-900">
+                    {/* <h3 className="sr-only">Categories</h3>
+                    {<ul role="list" className="px-2 py-3 font-medium text-gray-900">
                       {subCategories.map((category) => (
                         <li key={category.name}>
                           <a href={category.href} className="block px-2 py-3">
@@ -128,7 +170,7 @@ export default function Product() {
                           </a>
                         </li>
                       ))}
-                    </ul> */}
+                    </ul>} */}
 
                     {filters.map((section) => (
                       <Disclosure as="div" key={section.id} className="border-t border-gray-200 px-4 py-6">
@@ -151,6 +193,7 @@ export default function Product() {
                                 {section.options.map((option, optionIdx) => (
                                   <div key={option.value} className="flex items-center">
                                     <input
+                                      onChange={()=>handleFilter(option.value, section.id)}
                                       id={`filter-mobile-${section.id}-${optionIdx}`}
                                       name={`${section.id}[]`}
                                       defaultValue={option.value}
@@ -248,16 +291,20 @@ export default function Product() {
             </h2>
 
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-6">
-              {/* Filters */}
+              <div>
+              <div className='py-10 flex justify-between items-center'>
+                <h1 className='text-lg opacity-50 font-bold'>Filters</h1>
+                <FilterListIcon/>
+              </div>
               <form className="hidden lg:block">
-                <h3 className="sr-only">Categories</h3>
-                {/* <ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
+                {/* <h3 className="sr-only">Categories</h3>
+                {<ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
                   {subCategories.map((category) => (
                     <li key={category.name}>
                       <a href={category.href}>{category.name}</a>
                     </li>
                   ))}
-                </ul> */}
+                </ul>} */}
 
                 {filters.map((section) => (
                   <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
@@ -301,8 +348,47 @@ export default function Product() {
                     )}
                   </Disclosure>
                 ))}
-              </form>
+                {singleFilter.map((section) => (
+                  <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
+                    {({ open }) => (
+                      <>
+                        <h3 className="-my-3 flow-root">
+                          <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                            {/* <span className="font-medium"></span> */}
+                              <FormLabel sx={{color:"black"}} className='text-gray-900' id="demo-radio-buttons-group-label">{section.name}</FormLabel>
+                            <span className="ml-6 flex items-center">
+                              {open ? (
+                                <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                              ) : (
+                                <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                              )}
+                            </span>
+                          </Disclosure.Button>
+                        </h3>
 
+                        <Disclosure.Panel className="pt-6">
+                          <div className="space-y-4">
+                          <FormControl>
+                          <RadioGroup
+                                aria-labelledby="demo-radio-buttons-group-label"
+                                defaultValue="female"
+                                name="radio-buttons-group"
+                              >
+                            {section.options.map((option, optionIdx) => (
+                              <>
+                                <FormControlLabel onChange={(e)=>handleRadioFilterChange(e,section.id)} value={option.value} control={<Radio />} label={option.label} />
+                              </>
+                            ))}
+                            </RadioGroup>
+                            </FormControl>
+                          </div>
+                        </Disclosure.Panel>
+                        </>
+                    )}
+                  </Disclosure>
+                ))}
+              </form>
+              </div>
               {/* Product grid */}
               <div className="lg:col-span-5 w-full">
                 <div className='flex flex-wrap justify-center bg-white py-5'>
