f8728db8cc4b83e70bb348c63d86efbbd5a541ed
Saurav <phatesaurav@gmail.com>
2023-08-16 16:44:23 +0530

    completed checkout page

--- frontend/src/customer/components/AddressCard/AddressCard.jsx
+++ frontend/src/customer/components/AddressCard/AddressCard.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const AddressCard = () => {
   return (
-    <div>AddressCard</div>
+    <div>
+      <div className='space-y-3'>
+        <p className='font-semibold'>Rohit Kakde</p>
+        <p>13/2, Madhukunj Society, Panchawati, Pune - 411008.</p>
+        <div className='space-y-1'>
+          <p className='font-semibold'>Phone Number</p>
+          <p>0123456789</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
