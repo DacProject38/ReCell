80c4501637af5e4f64c10ef48460acf02bf49703
Aditya77p <97120130+Aditya77p@users.noreply.github.com>
2023-08-17 20:40:19 +0530

    implemented security

---
+++ backend/recell/src/main/java/com/recell/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.recell.config;
+
+public class JwtConstant {
+
+	public static final String SECRET_KEY = "djfjlkfamcmnvxnmcvbxcnmvbxgsdfskhfshfuirtoertoipjkfjkajkajbhjr";
+
+	public static final String JWT_HEADER = "Authorization";
+
+}
