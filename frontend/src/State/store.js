b747eb189b4c9a8ef8ffa69d845113e445e6a0c1
Akash Verma <omakash3110@gmail.com>
2023-08-23 23:34:08 +0530

    Revert "cart and product"
    This reverts commit 3e77e882ebcce935eb34eaacc8cb46b2b042e233.

--- frontend/src/State/store.js
+++ frontend/src/State/store.js
@@ -1,11 +1,9 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
-import { customerProductReducer } from "./Product/Reducer";
 
 const rootReducers=combineReducers({
-    auth:authReducer,
-    product:customerProductReducer,
+    auth:authReducer
 
 })
 
